3a7bd11c93ee225b15ff130b67eb45a7a762b295
phungvantrongnghia <phungvantrongnghia1@gmail.com>
2020-05-11 21:56:32 +0700

    upload

--- src/Controllers/document_share.js
+++ src/Controllers/document_share.js
@@ -32,7 +32,6 @@ const get_docs_share = async (tableName, Id, rela, tableName2) => {
 }
 const getNode = async (tableName, Id) => {
     let ss = driverNeo4j.session();
-    console.log(Id);
     let rs = await ss.run(`MATCH (a:${tableName} {Id:${Id}})return a`);
     return rs;
 }
@@ -42,8 +41,6 @@ const createNode = async (tableName, data) => {
     return rs;
 }
 module.exports.share_document = async (req, res) => {
-    let result = [];
-    console.log(req.body);
     const document = await selectData('documents', {
         filteringConditions: [
             ['Id', '=', req.body.id]
@@ -54,119 +51,83 @@ module.exports.share_document = async (req, res) => {
         message: "Document id is not exits!"
     })
     const Knex = knex();
-
     let userShare = await Knex.select('Id', 'FullName').from('user')
         .whereIn('Email', [...req.body.userShare])
     let idUser = userShare.map(item => item.Id)
     let documentNode = await getNode('document_share', req.body.id);
     let rsUser = await session.run(`MATCH (d:user) WHERE d.Id IN [${[...idUser]}]  RETURN d `);
-    console.log("rsUser>>>>", rsUser);
     let userExits = rsUser.records.map(item => ({ Id: item._fields[0].properties.Id.low, FullName: item._fields[0].properties.FullName }))
     let userNotExits = userShare.filter(item => userExits.findIndex(a => a.Id === item.Id) === - 1 ? item : "");
-    console.log("user Exit >>", userExits);
     let docShare = await get_docs_share('document_share', req.body.id, 'share', 'user');
-    console.log("doc share>>>>", docShare);
-
     let rsRelationShipOfNode = docShare.records.map(item => item._fields[1].properties.Id.low);
-    console.log("rs >>>", rsRelationShipOfNode);
     let temp = idUser.filter(item => rsRelationShipOfNode.findIndex(i => i === item) === -1 ? item : "");
-    // Check nếu temp không rỗng call tạo relationship của nó
-    /* Check đây nè ở dưới mở ra khi xong thoi
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    */
-    console.log("temp >>", temp);
-    console.log("userID>> ", idUser);
-    // console.log("loook >>>",docShare.records[0].keys);
+    if (userExits.length === 0) {
+        userShare.map(async item => {
+            let rs = await createNode('user', { Id: item.Id, FullName: item.FullName })
+        })
+    }
+    else {
+        if (userNotExits.length !== 0) {
+            userNotExits.map(async item => {
+                let rs = await createNode('user', { Id: item.Id, FullName: item.FullName })
+                console.log("dsada", rs);
+            })
+        }
+    }
+    let ss = driverNeo4j.session();
+    if (documentNode.records.length === 0) {
+        let rs = await session.run(`CREATE (a:document_share {Id:${document[0].Id},Fullname:${document[0].Title}})return a`);
+        if (rs.records.length === 0) return res.status(401).json({
+            status_code: 401,
+            message: "Share document is faild!"
+        })
+        let ses = driverNeo4j.session();
+        let userNode = [...idUser, ...temp];
+        setTimeout(() => {
+            ses.run(`MATCH (a:document_share {Id: ${document[0].Id}}), (b:user) WHERE b.Id IN [${[...userNode]}] MERGE (a)-[r:share]->(b) return a,b`).then(result => {
+            })
+        }, 2000)
+    }
+    else {
+        let userNode = [...userNotExits.map(item => item.Id), ...temp];
+        setTimeout(() => {
+            session.run(`MATCH (a:document_share {Id: ${document[0].Id}}), (b:user) WHERE b.Id IN [${[...userNode]}] MERGE (a)-[r:share]->(b) return a,b`).then(result => {
+            })
+        }, 2000)
 
-    // if (userExits.length === 0) {
-    //     userShare.map(async item => {
-    //         let rs = await createNode('user', { Id: item.Id, FullName: item.FullName })
-    //     })
-    // }
-    // else {
-    //     if (userNotExits.length !== 0) {
-    //         userNotExits.map(async item => {
-    //             let rs = await createNode('user', { Id: item.Id, FullName: item.FullName })
-    //             console.log("dsada", rs);
-    //         })
-    //     }
-    //     //         // 
-    //     //         // 
-    //     //         // MATCH (a:document_share {id: 1}), (b:user) // tao multi relationship
-    //     //         // WHERE b.Id IN [4,10]
-    //     //         // MERGE (a)-[r:share]->(b)
-    //     //         // 
-    // }
-    // let ss = driverNeo4j.session();
-    // if (documentNode.records.length === 0) {
-    //     let rs = await session.run(`CREATE (a:document_share {Id:${document[0].Id},Fullname:${document[0].Title}})return a`);
-    //     if (rs.records.length === 0) return res.status(401).json({
-    //         status_code: 401,
-    //         message: "Share document is faild!"
-    //     })
-
-    //     let ses = driverNeo4j.session();
-
-    //     setTimeout(() => {
-    //         ses.run(`MATCH (a:document_share {Id: ${document[0].Id}}), (b:user) WHERE b.Id IN [${[...idUser]}] MERGE (a)-[r:share]->(b) return a,b`).then(result => {
-    //             console.log(">>>>>", result);
-    //         })
-    //     }, 2000)
-    // }
-    // else {
-    //     console.log("đã tồn tại");
-    //     console.log();
-    //     console.log("user exit", userNotExits);
-    //     setTimeout(() => {
-    //         session.run(`MATCH (a:document_share {Id: ${document[0].Id}}), (b:user) WHERE b.Id IN [${[...userNotExits.map(item => item.Id)]}] MERGE (a)-[r:share]->(b) return a,b`).then(result => {
-    //             console.log(">>>>>", result);
-    //         })
-    //     }, 2000)
-
-    // }
-    /* Nếu doument === 0 tài liệu chưa share
-     * Nếu # 0 đã share chỉ thêm user được share
-     *
-     */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // console.log(document);
-
+    }
     res.send("Share document");
+}
+const deleteNode = async (tableName, Id) => {
+    let ss = driverNeo4j.session();
+    let rs = await ss.run(`MATCH (a:${tableName} {Id:${Id}}) DETACH DELETE a`);
+    return rs;
+}
+const deleteRelationShipNode = async (tableName, Id, relate) => {
+    let ss = driverNeo4j.session();
+    let rs = await ss.run(`MATCH (p:${tableName} {Id:${Id}}) MATCH (p)-[r:${relate}]-() DELETE r`);
+    return rs;
+}
+module.exports.dele_user_shared = async (req, res) => {
+    let delRe = await deleteRelationShipNode('user', 7, 'share');
+    let docs = await get_docs_share('user', 7, 'share', 'document_share');
+    console.log("delete >>", delRe);
+    if (docs.records.length === 0) {
+        let delNode = await deleteNode('user', 7)
+        console.log("delNode >>", delNode);
+    }
+    console.log("docs >>>", docs);
+    console.log(docs.records.length);
+    // Xóa user được chia sẻ tài liệu
+    // Input : [array user] , Id Document
+    //  get ID của arr User 
+    //  Delete relationship của arr user với DocsShare
+    // Check node user empty relationship => delete node user
+}
+module.exports.delete_docs_share = async (req, res) => {
+    // Delete docs share
+    // Input Id docs
+    // Get các node có relation với docs
+    // xóa tất cả quan hệ của docs và xóa docs
+    // Check từng node empty relationship => delete node
 }
--- src/Routes/document_share.js
+++ src/Routes/document_share.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { share_document,get_docs_share} = require("../Controllers/document_share");
+const { share_document,get_docs_share,dele_user_shared} = require("../Controllers/document_share");
 const { upload } = require("../utils/file");
 router.post('/share',share_document);
 router.get('/get_docs_share/:id',get_docs_share);
+router.delete('/delete-user-shared',dele_user_shared);
 // router.post('/create', upload, create)
 // router.put('/update', upload, update)
 // router.delete('/delete/:id', delete_document)
