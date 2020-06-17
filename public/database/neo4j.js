a0b7e676ed9e0f7bc6df7dd17d89fed68c20acb4
phungvantrongnghia <phungvantrongnghia1@gmail.com>
2020-06-17 09:50:18 +0700

    upload

--- public/database/neo4j.js
+++ public/database/neo4j.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver');
 
-let driver = neo4j.driver('neo4j://18.207.132.125:7687',neo4j.auth.basic('neo4j','123456'));
+let driver = neo4j.driver('neo4j://3.234.209.52:7687',neo4j.auth.basic('neo4j','123456'));
 
 module.exports = driver;
